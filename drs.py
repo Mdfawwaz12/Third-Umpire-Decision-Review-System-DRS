e9951c5331e17f40cb66aa99a0961ddad58469af
Fawwaz <98513981+Mdfawwaz12@users.noreply.github.com>
2023-09-30 23:00:53 +0530

    drs.py

--- drs.py
+++ drs.py
@@ -1,11 +1,10 @@
 import tkinter
 import cv2
 import PIL.Image,PIL.ImageTk #pil- python image library
-#imageTk- tkinter mein image dikhane use hotaso
+#imageTk- tkinter to show images
 from functools import partial
-import threading #tkinter mein mainloop chalna zaruri, usse thread banako chodalteso
-#image sob change karne vaste
-import imutils # pending func mein width,height ko resize karane
+import threading #for tkinter mainloop runs compulsory, so using of thread
+import imutils 
 import time
 
 
@@ -18,8 +17,8 @@ def play(speed):
     frame = var2.get(cv2.CAP_PROP_POS_FRAMES)
     var2.set(cv2.CAP_PROP_POS_FRAMES,frame + speed)
 
-    grabbed, frame = var2.read() # grabbed aako boolean var frame ko correct se liya ki nai ki
-    if not grabbed: # poora video hoge baad terminal mein error nai aana karko haiso
+    grabbed, frame = var2.read() 
+    if not grabbed: 
         exit()
     frame = imutils.resize(frame,width=Width,height=Height)
     frame= PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
@@ -49,7 +48,7 @@ def pending(decision):
 
 
 def out():
-    thread = threading.Thread(target=pending, args=("out",)) #args-tuple,args aako pending func ka argument
+    thread = threading.Thread(target=pending, args=("out",)) #args-tuple,args - pending functions argument
     thread.daemon = 1
     thread.start()
     print("out")
@@ -67,14 +66,10 @@ var.title("Third Umpire Decision Review System")
 cv_img = cv2.cvtColor(cv2.imread("welcome.jpg"), cv2.COLOR_BGR2RGB) #image insertion
 
 canva = tkinter.Canvas(var,width=Width, height=Height) 
-#width bhi height aako line6,7 ka number set kardal
 
 pic = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img)) 
-#line 14-.png ko canvas mein andar lane vaste photoImage. fromarray-image ko cv ka through se read karnaso
 
 image_on_canvas = canva.create_image(0,0,ancho= tkinter.NW, image=pic)
-#NW-north,west, image ko0,0 mein lagana. 
-
 canva.pack()
 
 #buttons to control
@@ -89,8 +84,6 @@ btn.pack()
 
 btn = tkinter.Button(var, text="Next (fast)",width=40, command=partial(play, 20)) # command katho func ka name liknaso
 btn.pack()
- #partial katho argument letaso, ab command=play(argument) aisa nai de sakte 
- #usse aako functool se partial ko import karko commad mein partial likko function name bhi argument deteso
 btn = tkinter.Button(var, text="Out",width=40,command=out)
 btn.pack()
 
